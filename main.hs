57746e5adede844c834e0e43ff2d0693db0a816a
Kelly Wang <wangz27@rpi.edu>
2019-09-21 23:39:01 -0400

    update 9/21

--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 import PA1Helper
 import System.Environment (getArgs)
+import Data.Map
 
 -- Haskell representation of lambda expression
 -- data Lexp = Atom String | Lambda String Lexp | Apply Lexp  Lexp 
@@ -24,12 +25,26 @@ id' lexp@(Apply _ _) = lexp
 -- return whatever it was given, of course!
 
 reducer :: Lexp -> Lexp
+reducer lexp1@(Apply lexp2@(Lambda old exp1) new) = (subst exp1 old new)
 reducer lexp = lexp
+ 
+
+subst :: Lexp -> String -> Lexp -> Lexp
+subst v@(Atom var1) old new = if var1 == old then new else v
+subst lexp@(Lambda var1 exp1) old new = (Lambda var1 (subst exp1 old new))
+subst lexp@(Apply exp1 exp2) old new = (Apply (subst exp1 old new) (subst exp2 old new))
+
+
+eta :: Lexp -> Lexp
+eta v@(Atom _) = v
+eta lexp@(Apply _ _) = lexp 
+eta lexp1@(Lambda var lexp2@(Apply exp1 v@(Atom var2))) = if var2 == var then exp1 else lexp1
+eta lexp@(Lambda _ _) = lexp
 
 -- Entry point of program
 main = do
     args <- getArgs
-    let inFile = case args of { x:_ -> x; _ -> "input.lambda" }
+    let inFile = case args of { x:_ -> x; _ -> "input1.lambda" }
     let outFile = case args of { x:y:_ -> y; _ -> "output.lambda"}
     -- id' simply returns its input, so runProgram will result
     -- in printing each lambda expression twice. 
